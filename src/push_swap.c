49ad74d44e9b76e8fe84a38785bb0adb8417280b
mateo-covacho <mateocovachov2@gmail.com>
2024-12-19 05:42:47 +0100

    asdasd

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,13 @@
 int main(int argc, char *argv[])
 {
   t_stack *stack_a;
-  /* t_stack *stack_b; */
+  t_stack *stack_b;
 
-  char* nums_string = parse_nums(argv);
+  char* nums_string = parse_nums(argv, stack_a);
   
   printf("nums_string: \"%s\" \n" , nums_string);
+
+  print_stacks(stack_a, stack_b);
   return 0;
 }
 
